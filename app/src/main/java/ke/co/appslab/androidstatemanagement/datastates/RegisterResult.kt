5bb8f0eca9337a8edc0507f2020afbbe1b605d9a
Harun Wangereka <harunwangereka@gmail.com>
2019-11-13 00:31:56 +0300

    added sample state management using sealed classes

--- app/src/main/java/ke/co/appslab/androidstatemanagement/datastates/RegisterResult.kt
+++ app/src/main/java/ke/co/appslab/androidstatemanagement/datastates/RegisterResult.kt
@@ -3,6 +3,15 @@ package ke.co.appslab.androidstatemanagement.datastates
 sealed class RegisterResult {
     data class Success(val data : String) : RegisterResult()
     object InvalidData :RegisterResult()
-    data class Error(val error : Throwable) : RegisterResult()
-    data class HttpErrors(val httpErrors : HttpErrorCodes) : RegisterResult()
+    data class Error(val error : String) : RegisterResult()
+    data class NetworkException(val error : String) : RegisterResult()
+    sealed class HttpErrors : RegisterResult() {
+        data class Unauthorized(val exception: String) : HttpErrors()
+        data class ResourceForbidden(val exception: String) : HttpErrors()
+        data class ResourceNotFound(val exception: String) :HttpErrors()
+        data class InternalServerError(val exception: String) :HttpErrors()
+        data class BadGateWay(val exception: String) : HttpErrors()
+        data class ResourceRemoved(val exception: String) : HttpErrors()
+        data class RemovedResourceFound(val exception: String) :HttpErrors()
+    }
 }
---
+++ app/src/main/java/ke/co/appslab/androidstatemanagement/ui/refactoredregister/RefactoredRegisterActivity.kt
@@ -0,0 +1,60 @@
+package ke.co.appslab.androidstatemanagement.ui.refactoredregister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ke.co.appslab.androidstatemanagement.R
+import ke.co.appslab.androidstatemanagement.datastates.RegisterResult
+import retrofit2.Response
+import java.lang.Exception
+
+class RefactoredRegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_refactored_register)
+
+    }
+
+    fun registerUser(fullName : String , email :String,password : String) : RegisterResult{
+        return try {
+            val response = registerRequest(fullName, email, password)
+            if (response!!.isSuccessful) {
+                if (response != null) {
+                    RegisterResult.Success(response.body()!!)
+                }else{
+                    RegisterResult.InvalidData
+                }
+            }else{
+                when(response.code()){
+                    401 -> RegisterResult.HttpErrors.Unauthorized(response.message())
+                    403 -> RegisterResult.HttpErrors.ResourceForbidden(response.message())
+                    404 -> RegisterResult.HttpErrors.ResourceNotFound(response.message())
+                    500 -> RegisterResult.HttpErrors.InternalServerError(response.message())
+                    502 -> RegisterResult.HttpErrors.BadGateWay(response.message())
+                    301 -> RegisterResult.HttpErrors.ResourceRemoved(response.message())
+                    302 -> RegisterResult.HttpErrors.RemovedResourceFound(response.message())
+                    else -> RegisterResult.Error(response.message())
+                }
+            }
+        }catch (error : Exception){
+            RegisterResult.NetworkException(error.message!!)
+        }
+    }
+
+    private fun showError(s: String) {
+
+    }
+
+    private fun proceedToDashBoard() {
+
+    }
+
+    private fun saveUserDetails(body: String?) {
+
+    }
+
+    fun registerRequest(fullName : String , email :String,password : String) : Response<String>? {
+
+        return null
+    }
+}
